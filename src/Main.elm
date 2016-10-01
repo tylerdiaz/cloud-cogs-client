69c354601d91e58913888ecc36b58d47982da095
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-01 12:06:06 +0300

    Header based on user
    This is jank, I'll refactor later

--- src/Main.elm
+++ src/Main.elm
@@ -20,13 +20,19 @@ main =
 -- model
 
 
+type alias User =
+    { username : String
+    , email : String
+    }
+
+
 type alias Model =
-    { quote : String
+    { user : Maybe User
     }
 
 
 init =
-    ( Model "", Cmd.none )
+    ( Model (Nothing), Cmd.none )
 
 
 
@@ -47,5 +53,62 @@ update msg model =
 -- view
 
 
+annonymousHeaderView =
+    header [ class "header" ]
+        [ div [ class "container" ]
+            [ nav [ class "nav" ]
+                [ div [ class "nav-left" ]
+                    [ a [ class "nav-item is-brand", href "#" ]
+                        [ strong [] [ text "cloud-cogs" ] ]
+                    ]
+                , span [ class "nav-toggle" ] [ span [] [], span [] [], span [] [] ]
+                , div [ class "nav-right nav-menu" ]
+                    [ a [ class "nav-item", href "#" ] [ text "Login" ]
+                    , a [ class "nav-item", href "#" ] [ text "Sign up" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+userHeaderView user =
+    header [ class "header" ]
+        [ div [ class "container" ]
+            [ nav [ class "nav" ]
+                [ div [ class "nav-left" ]
+                    [ a [ class "nav-item is-brand", href "#" ]
+                        [ strong [] [ text "cloud-cogs" ] ]
+                    ]
+                , span [ class "nav-toggle" ] [ span [] [], span [] [], span [] [] ]
+                , div [ class "nav-right nav-menu" ]
+                    [ a [ class "nav-item", href "#" ] [ text "Logout" ]
+                    , span [ class "nav-item" ] [ text user.email ]
+                    ]
+                ]
+            ]
+        ]
+
+
 view model =
-    div [ class "container" ] [ text "Hello world" ]
+    let
+        header =
+            case model.user of
+                Nothing ->
+                    annonymousHeaderView
+
+                Just user ->
+                    userHeaderView user
+    in
+        div [ class "container" ]
+            [ header
+            , div [ class "columns" ]
+                [ div [ class "column is-three-quarters" ]
+                    [ p [ class "" ]
+                        [ text "main map" ]
+                    ]
+                , div [ class "column" ]
+                    [ p [ class "" ]
+                        [ text "Your avatar info" ]
+                    ]
+                ]
+            ]
