1c6c7112b348d4dbab546f5603f7b2bc17a463c5
Tyler J. Diaz <me@tylerdiaz.com>
2016-10-08 21:56:48 +0300

    fix landing page styling a bit

--- src/LandingPage/View.elm
+++ src/LandingPage/View.elm
@@ -10,16 +10,18 @@ import LandingPage.Types exposing (Msg(..))
 
 
 rootView model =
-    div [ class "container landing-container" ]
-        [ div [ class "columns" ]
-            [ div [ class "column is-one-quarter" ]
-                [ img [ src "/images/logo.png", width 180 ] []
-                , p [ class "landing-description" ] [ text "You've wondered into something different. Life on the land has become too polluted, too contaminated. People getting sick. Can't live there anymore. This is your new home, an airship called 'Little Piston'. Come in, meet your new neighbors." ]
-                , p [ class "landing-description" ] [ text "Currently in Alpha v0.1" ]
-                ]
-            , div [ class "column is-offset-1" ]
-                [ Html.App.map RegisterAction (RegistrationView.rootView model.registerForm)
-                , Html.App.map LoginAction (LoginView.rootView model.loginForm)
+    div [ class "landing-body" ]
+        [ div [ class "container landing-container" ]
+            [ div [ class "columns" ]
+                [ div [ class "column is-one-quarter" ]
+                    [ img [ src "/images/logo.png", width 180 ] []
+                    , p [ class "landing-description" ] [ text "You've wondered into something different. Life on the land has become too polluted, too contaminated. People getting sick. Can't live there anymore. This is your new home, an airship called 'Little Piston'. Come in, meet your new neighbors." ]
+                    , p [ class "landing-description" ] [ text "Currently in Alpha v0.1" ]
+                    ]
+                , div [ class "column is-offset-1" ]
+                    [ Html.App.map RegisterAction (RegistrationView.rootView model.registerForm)
+                    , Html.App.map LoginAction (LoginView.rootView model.loginForm)
+                    ]
                 ]
             ]
         ]
